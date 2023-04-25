e8cd06caff92a13554c050a6c31479d5bd3bf90e
Devfrank94 <francescomurro94@gmail.com>
2023-04-25 11:40:55 +0200

    last commit

--- js/script.js
+++ js/script.js
@@ -8,36 +8,36 @@ createApp({
     return{
       apiUrl:'https://flynn.boolean.careers/exercises/api/random/mail',
 
-      mails: []
-  
+      mailList: [],
+      mailGenerate: null
     }
 
   },
 
   methods: {
+
+    // Funzione che genera e richiama array di mail
     getMails(){
+      for(let i = 0; i < 10; i++){
 
-      axios.get(this.apiUrl, {
-        params: {
-          results: 10
-        }
+      axios.get(this.apiUrl).then(response => {
+
+        this.mailGenerate = response.data.response;
+        console.log(this.mailGenerate)
+        this.mailList.push(this.mailGenerate)
+
       })
-      
-      .then(response => {
-        this.mails = response.data.response;
-        console.log(this.mails);
-        
-      })
-
-  },
-
+    }
+    
+    console.log(this.mailList)
+  }
 },
 
   mounted(){
 
-    this.getMails();
+    this.getMails()
 
+  }
 
-  },
 
 }).mount('#app')
